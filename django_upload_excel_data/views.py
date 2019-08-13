18e53f0389c4dc96d854dfec81fa069871225adc
Haron Ongaki <50439917+haronongaki@users.noreply.github.com>
2019-08-13 17:57:15 +0300

    Version 0.0.1
    Initial commit

---
+++ django_upload_excel_data/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import openpyxl
+
+
+def index(request):
+    if "GET" == request.method:
+        return render(request, 'index.html', {})
+    else:
+        excel_file = request.FILES["excel_file"]
+
+        # you may put validations here to check extension or file size
+
+        wb = openpyxl.load_workbook(excel_file)
+
+        # getting a particular sheet by name out of many sheets
+        worksheet = wb["Sheet1"]
+        print(worksheet)
+
+        excel_data = list()
+        # iterating over the rows and
+        # getting value from each cell in row
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+        return render(request, 'index.html', {"excel_data":excel_data})
